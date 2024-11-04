7dadbf8d8d853daf93024baee89a52d4de7b158d
Yasoob <kamiliyasoob@gmail.com>
2024-11-04 23:38:54 +0530

    2nd program pushed

---
+++ LL_insertion_at_end.c
@@ -0,0 +1,48 @@
+// multiple insertions at the end of the linked list
+// CSE-23-46
+#include<stdio.h>
+#include<stdlib.h>
+void insert_node(int value);  // functioni to insert nodes at the end
+struct Node{
+    int data;
+    struct Node* next;
+};
+struct Node* A = NULL ;
+
+int main(){
+    int num,i,value;
+    printf("How many values?\n");
+    scanf("%d",&num);
+    for(i=0;i<num;i++){
+        printf("Enter value %d\n",i+1);
+        scanf("%d",&value);
+        insert_node(value);
+    }
+    //printing linked list nodes one by one
+    struct Node* current = A;
+	while(current != NULL){
+		printf("Data is %d\nNext is %p\n",current->data,current->next);
+		current = current->next;
+    }
+		return 0;
+}
+
+void insert_node(int value){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = value;
+    temp->next = NULL;
+
+    if(A == NULL){
+        A = temp;
+    }
+    else{
+        struct Node* current = A;
+        while(current->next != NULL){
+            current = current->next;
+        }
+        current->next = temp;        
+    }
+    
+    return 0;
+     
+}
