1eb249f4a41a7f5cb5ebff8ce245cd9fd53cc9b2
Yasoob <kamiliyasoob@gmail.com>
2025-05-02 23:02:35 +0530

    Update 1st_and_last_position.py

--- Python_DSA/1st_and_last_position.py
+++ Python_DSA/1st_and_last_position.py
@@ -1,6 +1,15 @@
 # Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
 # If target is not found in the array, return [-1, -1].
 # Leetcode 
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        return first_and_last(nums,target)
+    
 def binary_search(low, high,condition):
     while low<=high:
         mid = (low+high)//2
@@ -25,6 +34,7 @@ def first_pos_test(nums,target):
         else:
             return 'right'
     return binary_search(0,(len(nums)-1),condition)
+
 def last_pos_test(nums,target):
     def condition(mid):
         if nums[mid] == target:
@@ -37,13 +47,9 @@ def last_pos_test(nums,target):
         else:
             return 'right'
     return binary_search(0,len(nums)-1,condition)
+
 def first_and_last(nums,target):
     return first_pos_test(nums,target), last_pos_test(nums,target)
-class Solution(object):
-    def searchRange(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        return first_and_last(nums,target)
+
+s1 = Solution()
+print(s1.searchRange([1,2,3,5,5,5,5,6,7,8],5))
